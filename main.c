57b049574d7160513988c9842147d5ea426cbde9
adele-martin <adelejulietmartin@gmail.com>
2024-05-22 18:12:59 +0200

    ok

--- main.c
+++ main.c
@@ -1,43 +1,21 @@
 #include "fractol.h"
 
+/*
 void mandelbrot_set(t_fractal *fractal, double x_min, double x_max, double y_min, double y_max)
 {
 	//a + bi
+	int z = 
 	while (z < 41)
 	{
-		z = (z * z)+
+		z = (z * z);
 	}
 	//(a + bi)² or (a + bi) * (a + bi)
 }
 
 int handle_arguments(t_fractal *fractal, char **argv)
 {
-	fractal->name = av[1];
+	fractal->name = argv[1];
 
-}
-
-int fractal_init(t_fractal *fractal)
-{
-	fractal->mlx_ptr = mlx_init();
-	if (!fractal->mlx_ptr)
-		return (1);
-
-	fractal->win = mlx_new_window(fractal->mlx_ptr, WIDTH, HEIGHT, "fractal window");
-	if (fractal->win == NULL)
-	{
-		mlx_destroy_display(fractal->mlx_ptr);
-		free(fractal->mlx_ptr);
-		return (1);
-	}
-	//creates a blank image to be drawn into
-	fractal->img.img_ptr = mlx_new_image(fractal->mlx_ptr, WIDTH, HEIGHT);
-	//protect it here
-
-	//allows us to modify the image later
-	fractal->img.pixel = mlx_get_data_addr(fractal->img.img_ptr,
-	&fractal->img.bpp, &fractal->img.line_len, &fractal->img.endian);
-
-	return (0);
 }
 
 void pixels_to_screen(t_fractal *fractal)
@@ -57,16 +35,43 @@ void pixels_to_screen(t_fractal *fractal)
 	}
 
 }
+*/
+int fractal_init(t_fractal *fractal)
+{
+	fractal->mlx_ptr = mlx_init();
+	if (!fractal->mlx_ptr)
+		return (1);
+
+	fractal->win = mlx_new_window(fractal->mlx_ptr, WIDTH, HEIGHT, "fractal window");
+	if (fractal->win == NULL)
+	{
+		mlx_destroy_display(fractal->mlx_ptr);
+		free(fractal->mlx_ptr);
+		return (1);
+	}
+	//creates a blank image to be drawn into
+	fractal->image.img_ptr = mlx_new_image(fractal->mlx_ptr, WIDTH, HEIGHT);
+	//protect it here
+
+	//allows us to modify the image later
+	fractal->image.pixel = mlx_get_data_addr(fractal->image.img_ptr,
+	&fractal->image.bpp, &fractal->image.line_len, &fractal->image.endian);
+
+	return (0);
+}
+
+
 
 int main(int argc, char **argv)
 {
 	t_fractal	fractal;
 	if (argc == 2)
 	{
-	handle_arguments(&argv, &fractal);
+	//handle_arguments(&argv, &fractal);
 	fractal_init(&fractal);
-	mandelbrot_set(&fractal, -2, 2, -2, 2);
+	//mandelbrot_set(&fractal, -2, 2, -2, 2);
 	//pixels_to_screen(&fractal);
+	mlx_loop(fractal.mlx_ptr);
 	(void)argv;
 	mlx_destroy_window(fractal.mlx_ptr, fractal.win);
 	mlx_destroy_display(fractal.mlx_ptr);
