8ae695eafe17d9a4dfe2ee054a77c0cc1ca2dc79
moldypeaches <adelejulietmartin@gmail.com>
2024-05-30 11:32:14 +0200

    ok

--- color.c
+++ color.c
@@ -30,9 +30,21 @@ int get_color(int iter, int max_iter)
     int color;
     color = iter * 25;
 	(void)max_iter;
+	r = get_r(color);
+    g = get_g(color);
+    b = get_b(color);
+
+    // Increase brightness by scaling the RGB values
+    r = r + (255 - r) * 0.5;
+    g = g + (255 - g) * 0.5;
+    b = b + (255 - b) * 0.5;
+
+    return create_trgb(get_t(color), r, g, b);
+	//return(create_trgb(get_t(color), get_r(color), get_g(color), get_b(color)));
+}
+
+
 	//int r = (int)(9 * (1 - t) * t * t * t * 255);
     //int g = (int)(15 * (1 - t) * (1 - t) * t * t * 255);
     //int b = (int)(8.5 * (1 - t) * (1 - t) * (1 - t) * t * 255);
     //return create_trgb(0, r, g, b);
-	return(create_trgb(get_t(color), get_r(color), get_g(color), get_b(color)));
-}
